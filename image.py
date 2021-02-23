82849a1eb2c1e4e1bfcc05621b68531f4f7d298f
Jonathan Smith <77024577+thejonathanesmith@users.noreply.github.com>
2021-02-22 22:16:30 -0600

    Update image.py

--- image.py
+++ image.py
@@ -4,15 +4,11 @@ from PIL import Image
 #Retriving all image names and it's path with .jpg extension from given directory path in imageNames list
 imageNames = glob.glob(r"C:\Users\Me\Desktop\test\*.jpg")
 
-#Defining width and height of image
-#new_width  = 800
-#new_height = 600
 
 #Count variable to show the progress of image resized
 count=0
 
-#Creating for loop to take one im
-age from imageNames list and resize
+#Creating for loop to take one image from imageNames list and resize
 for i in imageNames:
     img = Image.open(i)
     img = img.resize((img.width//2, img.height//2))
